a9c15289f16d59a34182974b012d2d930ec7a747
Skyler Rexroad <skyrex1095@gmail.com>
2015-12-04 08:35:43 -0700

    Update call to action

--- php/lib/call-to-action.php
+++ php/lib/call-to-action.php
@@ -1,17 +1,17 @@
 <div class="call-to-action-bg animated fadeIn">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="call-to-action">
-                    <h1>
-                        Hi, I'm Skyler.
-                        <br/>
-                        <small>Musician, web developer, programmer.</small>
-                        <br/>
-                        <small>Go ahead, take a scroll.</small>
-                    </h1>
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="call-to-action">
+					<h1>
+						Hi, I'm Skyler.
+						<br/>
+						<small>Web designer, developer, and geek.</small>
+						<br/>
+						<small>Go on, take a scroll.</small>
+					</h1>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
