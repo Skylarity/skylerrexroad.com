8e0eaba95698d0d4927db26ded29229bd1b718f0
Skyler Rexroad <skyrex1095@gmail.com>
2015-12-04 08:11:52 -0700

    Change spaces to tabs, apparently

--- index.php
+++ index.php
@@ -3,14 +3,14 @@ $CURRENT_DIR = __DIR__;
 $PAGE_TITLE = "Skyler Rexroad";
 ?>
 <?php require_once("php/lib/head-utils.php"); ?>
-    <body class="sfooter">
-        <div class="sfooter-content">
-            <?php require_once("php/lib/header.php"); ?>
+	<body class="sfooter">
+		<div class="sfooter-content">
+			<?php require_once("php/lib/header.php"); ?>
 
-            <?php require_once("php/lib/call-to-action.php"); ?>
+			<?php require_once("php/lib/call-to-action.php"); ?>
 
-            <?php require_once("php/lib/content.php"); ?>
-        </div>
-        <?php require_once("php/lib/footer.php"); ?>
-    </body>
+			<?php require_once("php/lib/content.php"); ?>
+		</div>
+		<?php require_once("php/lib/footer.php"); ?>
+	</body>
 <?php require_once("php/lib/foot-utils.php"); ?>
