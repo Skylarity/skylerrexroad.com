3bca875064c2f6b05fb6e000c00f410e1c03ff90
Skyler Rexroad <skyrex1095@gmail.com>
2016-06-07 17:03:18 -0600

    Formatting

--- public_html/index.php
+++ public_html/index.php
@@ -4,5 +4,5 @@ $PAGE_TITLE = "Skyler Rexroad";
 ?>
 <?php require_once("lib/partials/head-utils.php"); ?>
 <?php require_once("lib/partials/header.php"); ?>
-<div class="container"><h1>Skyler Rexroad</h1></div>
+	<div class="container"><h1>Skyler Rexroad</h1></div>
 <?php require_once("lib/partials/foot-utils.php"); ?>
