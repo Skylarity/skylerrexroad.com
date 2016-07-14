408d7cfc35f800713b8765c9841e5c2ab713a6f6
Skyler Rexroad <skyrex1095@gmail.com>
2016-07-14 16:29:32 -0600

    Adjusted navbar title padding

--- public_html/lib/js/navbar.js
+++ public_html/lib/js/navbar.js
@@ -10,7 +10,7 @@ $(window).on('scroll', function() {
 		$('.navbar-default .navbar-brand').css({fontSize: "4rem", lineHeight: "20px"});
 		// $('.robo-logo').css({height: "50px"});
 	} else {
-		$('.navbar').css({paddingTop: "50px", background: "none", borderBottom: "1px solid #fff"});
+		$('.navbar').css({paddingTop: "20px", background: "none", borderBottom: "1px solid #fff"});
 		$('.navbar-default .navbar-nav > li > a').css({borderTopLeftRadius: "4px", borderTopRightRadius: "4px"});
 		$('.navbar-default .navbar-brand').css({fontSize: "8rem", lineHeight: "0"});
 		// $('.robo-logo').css({height: "75px"});
