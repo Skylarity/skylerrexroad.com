5abdd419e7b8a4fbed8f5d1f1543f10dc88fb94f
Skyler Rexroad <skyrex1095@gmail.com>
2016-07-15 08:51:56 -0600

    Added embedded SC player

--- public_html/index.php
+++ public_html/index.php
@@ -68,6 +68,12 @@ $PAGE_TITLE = "Skyler Rexroad";
 				</div>
 			</div>
 		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<iframe width="100%" height="450" scrolling="no" frameborder="no"
+						src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/8186758&amp;color=508cff&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false"></iframe>
+			</div>
+		</div>
 	</div>
 </div>
 <div id="contact" class="contact">
